a14202b66c59b2d8ceb4d36b5f85a392883fe9f2
tharusha-nax <tharusha.nax@gmail.com>
2024-06-25 16:04:24 +0530

    finished bottom tab & header

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,20 +1,53 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+import { Stack } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import Colors from "@/constants/Colors";
 
 const Page = () => {
   return (
-    <View style={styles.container}>
-      <Text>Page</Text>
-    </View>
-  )
-}
+    <Stack.Screen
+      options={{
+        headerTransparent: true,
+        headerTitle: "",
+        headerLeft: () => (
+          <TouchableOpacity style={{ marginLeft: 20 }}>
+            <Image
+              source={{
+                uri: "https://xsgames.co/randomusers/avatar.php?g=female",
+              }}
+              style={{ width: 40, height: 40, borderRadius: 10 }}
+            />
+          </TouchableOpacity>
+        ),
+        headerRight: () => (
+          <TouchableOpacity
+            onPress={() => {}}
+            style={{
+              marginRight: 20,
+              backgroundColor: Colors.white,
+              padding: 10,
+              borderRadius: 10,
+              shadowColor: "#171717",
+              shadowOffset: { width: 2, height: 4 },
+              shadowOpacity: 0.2,
+              shadowRadius: 3,
+            }}
+          >
+            <Ionicons name="notifications" size={20} color={Colors.black} />
+          </TouchableOpacity>
+        ),
+      }}
+    />
+  );
+};
 
-export default Page
+export default Page;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-})
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
