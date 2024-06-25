c8ff999a5ec270cce74e8bcd3bc0e17332c80618
tharusha-nax <tharusha.nax@gmail.com>
2024-06-25 17:07:45 +0530

    fixed shadow & added CategoryButtons

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,44 +1,79 @@
-import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React from "react";
 import { Stack } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
 import Colors from "@/constants/Colors";
+import { useHeaderHeight } from "@react-navigation/elements";
+import { SearchBar } from "react-native-screens";
+import CategoryButtons from "@/components/CategoryButtons";
 
 const Page = () => {
+  const headerHeight = useHeaderHeight();
+
   return (
-    <Stack.Screen
-      options={{
-        headerTransparent: true,
-        headerTitle: "",
-        headerLeft: () => (
-          <TouchableOpacity style={{ marginLeft: 20 }}>
-            <Image
-              source={{
-                uri: "https://xsgames.co/randomusers/avatar.php?g=female",
+    <>
+      <Stack.Screen
+        options={{
+          headerTransparent: true,
+          headerTitle: "",
+          headerLeft: () => (
+            <TouchableOpacity style={{ marginLeft: 20 }}>
+              <Image
+                source={{
+                  uri: "https://xsgames.co/randomusers/avatar.php?g=female",
+                }}
+                style={{ width: 40, height: 40, borderRadius: 10 }}
+              />
+            </TouchableOpacity>
+          ),
+          headerRight: () => (
+            <TouchableOpacity
+              onPress={() => {}}
+              style={{
+                marginRight: 20,
+                backgroundColor: Colors.white,
+                padding: 10,
+                borderRadius: 10,
+                shadowColor: "#171717",
+                shadowOffset: { width: 2, height: 4 },
+                shadowOpacity: 0.2,
+                shadowRadius: 3,
+                // elevation: 5
               }}
-              style={{ width: 40, height: 40, borderRadius: 10 }}
+            >
+              <Ionicons name="notifications" size={20} color={Colors.black} />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <View style={[styles.container, { paddingTop: headerHeight }]}>
+        <Text style={styles.headingText}>Explore The Beautiful World!</Text>
+
+        <View style={styles.searchSectionWrapper}>
+          <View style={styles.searchBar}>
+            <Ionicons
+              name="search"
+              size={18}
+              style={{ marginRight: 5 }}
+              color={Colors.black}
             />
+            <TextInput placeholder="Search..." />
+          </View>
+          <TouchableOpacity onPress={() => {}} style={styles.filterBtn}>
+            <Ionicons name="options" size={28} color={Colors.white} />
           </TouchableOpacity>
-        ),
-        headerRight: () => (
-          <TouchableOpacity
-            onPress={() => {}}
-            style={{
-              marginRight: 20,
-              backgroundColor: Colors.white,
-              padding: 10,
-              borderRadius: 10,
-              shadowColor: "#171717",
-              shadowOffset: { width: 2, height: 4 },
-              shadowOpacity: 0.2,
-              shadowRadius: 3,
-            }}
-          >
-            <Ionicons name="notifications" size={20} color={Colors.black} />
-          </TouchableOpacity>
-        ),
-      }}
-    />
+        </View>
+      </View>
+
+      <CategoryButtons />
+    </>
   );
 };
 
@@ -46,8 +81,30 @@ export default Page;
 
 const styles = StyleSheet.create({
   container: {
+    paddingHorizontal: 20,
+    backgroundColor: Colors.bgColor,
+  },
+  headingText: {
+    fontSize: 28,
+    fontWeight: "800",
+    marginTop: 10,
+  },
+  searchSectionWrapper: {
+    flexDirection: "row",
+    marginVertical: 20,
+  },
+  searchBar: {
     flex: 1,
-    justifyContent: "center",
+    flexDirection: "row",
+    backgroundColor: Colors.white,
+    padding: 10,
+    borderRadius: 10,
     alignItems: "center",
+  },
+  filterBtn: {
+    backgroundColor: Colors.primaryColor,
+    padding: 12,
+    borderRadius: 10,
+    marginLeft: 20,
   },
 });
